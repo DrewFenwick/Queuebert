d481276e7eef4eeb0442543f561cea5a38cc6ee2
DrewFenwick <drew.e.fenwick+github@gmail.com>
2020-05-07 12:05:06 +0100

    Define MonadReads constraint

--- app/Env.hs
+++ app/Env.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Env
   ( Env (..),
+    MonadReads,
   )
 where
 
+import Control.Monad.Reader
 import Servant.Client (ClientEnv)
 import Web.Telegram.API (Token (..))
 
 data Env = Env {token :: Token, clientEnv :: ClientEnv}
+
+type MonadReads env m = (MonadReader m, EnvType m ~ env)
