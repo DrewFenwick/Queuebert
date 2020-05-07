3c8b17603bddff2a353ff74647a596fe6c9752e8
DrewFenwick <drew.e.fenwick+github@gmail.com>
2020-05-07 13:04:53 +0100

    Define main

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import Control.Concurrent
+import Control.Monad.Reader
+import Env
+import Listener
+import Poster
+
 main :: IO ()
-main = undefined
+main = runApp app
+
+app :: App ()
+app = do
+  _ <- mapReaderT forkIO poster
+  listener
