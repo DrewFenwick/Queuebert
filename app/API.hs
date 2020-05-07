a6bce29fa6270faff3e6700bdc452edf33db2455
DrewFenwick <drew.e.fenwick+github@gmail.com>
2020-05-07 12:42:06 +0100

    Define reader-y runClientM function

--- app/API.hs
+++ app/API.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module API
   ( getUpdates,
+    runClientM,
   )
 where
 
+import Control.Monad.Reader
 import Data.Proxy
+import Env (Env, MonadReads)
+import qualified Env
 import Servant.Client (ClientError, ClientM)
 import qualified Servant.Client as Servant
 import Web.Telegram.API
@@ -16,3 +22,11 @@ api = Proxy
 
 getUpdates :: Token -> Polling -> ClientM (ReqResult [Update])
 getUpdates = Servant.client api
+
+runClientM ::
+  (MonadReads Env m, MonadIO m) =>
+  ClientM a ->
+  m (Either ClientError a)
+runClientM clientM = do
+  cliEnv <- asks Env.clientEnv
+  liftIO $ Servant.runClientM clientM cliEnv
