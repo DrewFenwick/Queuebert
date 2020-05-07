7696231ccbb2486ee8bde46949ed21a6b806de37
DrewFenwick <drew.e.fenwick+github@gmail.com>
2020-05-07 12:07:01 +0100

    Create runApp function and App type synonym

--- app/Env.hs
+++ app/Env.hs
@@ -4,13 +4,29 @@
 module Env
   ( Env (..),
     MonadReads,
+    App,
+    runApp,
   )
 where
 
 import Control.Monad.Reader
+import Data.Text (pack)
 import Servant.Client (ClientEnv)
+import System.Environment (getEnv)
 import Web.Telegram.API (Token (..))
 
 data Env = Env {token :: Token, clientEnv :: ClientEnv}
 
 type MonadReads env m = (MonadReader m, EnvType m ~ env)
+
+type App a = ReaderT Env IO a
+
+runApp :: App a -> IO a
+runApp app = do
+  env <- mkEnv
+  runReaderT app env
+
+mkEnv :: IO Env
+mkEnv = do
+  tok <- Token . pack <$> getEnv "QueuebertToken"
+  pure $ Env tok undefined
