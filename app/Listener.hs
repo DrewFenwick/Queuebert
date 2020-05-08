dca18fffe6a15b09f1781ef55cb284081e9b35ce
DrewFenwick <drew.e.fenwick+github@gmail.com>
2020-05-08 03:26:08 +0100

    Flesh out listener

--- app/Listener.hs
+++ app/Listener.hs
@@ -3,25 +3,40 @@
 module Listener where
 
 import API
+import Control.Concurrent (threadDelay)
 import Control.Monad.Reader
-import Data.Foldable
+import Data.Functor (($>))
 import Env (Env, MonadReads)
 import qualified Env
+import Web.Telegram.API (Token)
 import Web.Telegram.API.Update (Polling (..))
-import Web.Telegram.Types.Update (ReqResult (..))
+import Web.Telegram.Types.Update (ReqResult (..), Update)
 
 listener :: (MonadIO m, MonadReads Env m) => m ()
 listener = do
-  token <- asks Env.token
-  result <- runClientM $ getUpdates token polling
-  case result of
-    Right (Ok updates) -> liftIO $ traverse_ (print) updates
-    Left err -> liftIO $ print err
+  go <- asks (listenerLoop . Env.token)
+  uid <- getLastUpdate
+  go uid
 
-polling :: Polling
-polling =
+listenerLoop :: (MonadIO m, MonadReads Env m) => Token -> Int -> m ()
+listenerLoop token uid = do
+  result <- runClientM $ getUpdates token (polling uid)
+  newUid <- case result of
+    Right (Ok updates) -> store updates $> undefined updates
+    Left err -> liftIO (print err) $> uid
+  liftIO (threadDelay 10000)
+  listenerLoop token newUid
+
+store :: (MonadIO m, MonadReads Env m) => [Update] -> m ()
+store = undefined
+
+getLastUpdate :: (MonadIO m, MonadReads Env m) => m Int
+getLastUpdate = undefined
+
+polling :: Int -> Polling
+polling n =
   Polling
-    { offset = Nothing,
+    { offset = Just n,
       limit = Nothing,
       timeout = Nothing,
       allowedUpdates = Nothing
